db3efa8e7186e83309edcae0030444436c7e42bd
Luis San Martin <luis@sanmartin.io>
2020-12-04 12:50:09 +0100

    ansible inventory

--- hack/create-env.sh
+++ hack/create-env.sh
@@ -16,10 +16,17 @@ EOF
 
 for i in $(seq 1 3); do 
     sudo ignite run pathcl/ubuntu:18.04.5 --name vks0$i --cpus 2  --ssh=/home/pathcl/.ssh/id_rsa.pub --memory 2GB --size 8G;
-    sudo ignite inspect vm vks0$i |jq '.' | jq -r '.status.network.ipAddresses[]' >> hosts.ini;
+    sudo ignite inspect vm vks0$i |jq '.' | jq -r '[.metadata.name, .status.network.ipAddresses[]]| @csv'| sed 's/,/ ansible_host=/g;s/"//g' >> hosts.ini
 done
 
 
 # Master tls/crt replication 
 ansible -i hosts.ini -m copy -a 'src=../vks dest=/usr/sbin/vks' all
 ansible -i hosts.ini -m shell -a 'chmod a+x /usr/sbin/vks' all
+
+# now we should start k3s somehow
+
+ansible -i hosts.ini vks.yml
+
+# copy certs for rest of nodex
+# launch vks
